b0bc34900323e02a7d4da943d083ca7cbb1ddbaa
igrell <93985679+igrell@users.noreply.github.com>
2023-12-29 16:05:07 +0100

    Cleanup

--- test.hs
+++
@@ -1,8 +0,0 @@
-main = do
-    text <- readFile "input.txt"
-    let
-        readparse txtLines = map read txtLines :: [(Int,Int)]
-        parse txtLines = map words (lines txtLines)
-        pcval = map readparse (parse text) 
-    print pcval
-    return ()
